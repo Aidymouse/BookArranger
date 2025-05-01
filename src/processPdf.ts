07addcc859931bb2e050d8086572e12a8b436193
Aidymouse <aidan@thewizardscat.co.nz>
2025-05-02 00:48:24 +1200

    [ProcessPdf] correct logic for y placement

--- src/processPdf.ts
+++ src/processPdf.ts
@@ -14,6 +14,11 @@ export const rearrange_pdf = async (
   let somePassed = true;
   let latestPageSeen = -1;
 
+  // Create the page
+  // Iterate through the placements
+  // If there is no source page, mark the placement as complete
+  // If all placements are complete, do not add the page (optional in future)
+  
   while (somePassed === true) {
     somePassed = false;
 
@@ -21,27 +26,27 @@ export const rearrange_pdf = async (
       debug && console.log("New Page")
       let newPage = null;
 
-      for (const arrange_tile of arrange_page.tiles) {
-        const pgNumber = arrange_tile.start + arrange_tile.iteration + (arrange_tile.skip*arrange_tile.iteration) 
+      for (const placement of arrange_page.tiles) {
+        const source_page = placement.start + placement.iteration + (placement.skip*placement.iteration) 
 
-        arrange_tile.iteration += 1;
+        placement.iteration += 1;
 
         //latestPageSeen = Math.max(pgNumber, latestPageSeen);
 
-        if (pgNumber >= source.getPageCount()) continue;
+        if (source_page >= source.getPageCount()) continue;
 
-        debug && console.log("Drawing Source Page", pgNumber)
+        debug && console.log("Drawing Source Page", source_page)
 
         if (!newPage) newPage = result.addPage([arrange_page.width, arrange_page.height])
 
-        const sourcePage = source.getPage(pgNumber);
+        const sourcePage = source.getPage(source_page);
 
         if (!sourcePage) continue;
 
         const sourceEmbedded = await result.embedPage(sourcePage);
         newPage!.drawPage(sourceEmbedded, {
-          x: arrange_tile.position.x,
-          y: -arrange_tile.position.y
+          x: placement.position.x,
+          y: arrange_page.height - sourcePage.getHeight() - placement.position.y // PDF 0, 0 is BOTTOM left, pdf tile assumes 0, 0 at TOP left
         })
 
         somePassed = true;
